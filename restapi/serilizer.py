73ad57f51d943d0c21b04d0ff51f0c6670e4a347
Sameer Chauhan <sameerchauhan212204@gmail.com>
2025-04-21 17:11:26 +0530

    adding custom validation

--- restapi/serilizer.py
+++ restapi/serilizer.py
@@ -6,6 +6,18 @@ class StudentSerializer(serializers.ModelSerializer):
 
     class Meta: 
         model = Student 
-        # fields = '__all__'
+        fields = '__all__'
         # fields = ['name' , 'age']
-        exclude = ['id']
+        # exclude = ['id']
+
+
+# adding coustom validations in serliazer
+    def validate(self , data):
+        if data['age'] < 18:
+            raise serializers.ValidationError({'error':"age cannot be less than 18"})
+    
+        if data['name']:
+            for i in data['name']:
+                if i.isdigit():
+                    raise serializers.ValidationError({'Error':"name cant have number"})
+        return data
